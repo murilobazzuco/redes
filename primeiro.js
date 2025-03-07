8e7e53eefb56b5f8718032f6d0946845d3d598a4
murilobazzuco <murilosbhl@gmail.com>
2025-03-07 14:30:35 -0300

    modificando soma conflitp

--- primeiro.js
+++ primeiro.js
@@ -1,4 +1,6 @@
 const primeiroNumero = 1;
 const segundoNumero = 200;
+var numeroTres = -9;
 const soma = primeiroNumero + segundoNumero;
-console.log(soma);
+const subtracao = primeiroNumero - segundoNumero - numeroTres;
+console.log("Soma: ", soma, "Subtração: ", subtracao);
