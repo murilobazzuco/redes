ec50da1900e374be0fc2a04ad4c9815b539a1110
murilobazzuco <murilosbhl@gmail.com>
2025-03-07 10:47:49 -0300

    criando html e modfied soma

--- primeiro.js
+++ primeiro.js
@@ -1,4 +1,5 @@
 const primeiroNumero = 1;
 const segundoNumero = 200;
-const soma = primeiroNumero + segundoNumero;
+const terceiroNumero = 1000;
+const soma = primeiroNumero + segundoNumero + terceiroNumero;
 console.log(soma);
